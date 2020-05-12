ac43a80594440d7d0f4dc60e3dae3f2bc95d5350
Angus <1538997+angus151@users.noreply.github.com>
2020-05-13 09:32:07 +1000

    removed conditional import

--- openfisca_nsw_ess_nabers/variables/energy_savings_scheme/NABERS_offices/nabers_office_shared_library_of_terms.py
+++ openfisca_nsw_ess_nabers/variables/energy_savings_scheme/NABERS_offices/nabers_office_shared_library_of_terms.py
@@ -6,10 +6,10 @@ import numpy as np
 from openfisca_nsw_ess_nabers.variables.energy_savings_scheme.general_ESS.hidden_figures import get_parameters
 
 private = True
-if private:
+#if private:
     from openfisca_nsw_ess_nabers.variables.energy_savings_scheme.NABERS_offices import coefficient_values as c
-else:
-    from openfisca_nsw_ess_nabers.variables.energy_savings_scheme.NABERS_offices import public_coefficient_values as c
+#else:
+#    from openfisca_nsw_ess_nabers.variables.energy_savings_scheme.NABERS_offices import public_coefficient_values as c
 
 if not(c.has_real_values):
     error = ("real coefficients aren't used, and return values will be incorrect")
