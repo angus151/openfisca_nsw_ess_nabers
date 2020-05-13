58c314a3a18ad80345dd13861c7938784930548d
Angus <1538997+angus151@users.noreply.github.com>
2020-05-13 12:37:27 +1000

    update formula

--- openfisca_nsw_ess_nabers/variables/energy_savings_scheme/NABERS_ESS/step_two.py
+++ openfisca_nsw_ess_nabers/variables/energy_savings_scheme/NABERS_ESS/step_two.py
@@ -80,7 +80,7 @@ class method_one(Variable):
         built_before_or_after_nov_2006 = where(buildings('built_after_nov_2006', period),
         "built_after_nov_2006",
         "built_before_nov_2006")
-        if (current_rating_year >= parameters(period).energy_savings_scheme.table_a20.min_year):
+        if ((current_rating_year >= parameters(period).energy_savings_scheme.table_a20.min_year).any()):
             year_count = parameters(period).energy_savings_scheme.table_a20.min_year - 1
             while (year_count < current_rating_year):
                 year_count += 1
